94c57bfb7b4ce9ed1922640106c2970c611d38b0
chen-cx <1667156977@qq.com>
2020-03-17 11:57:13 +0800

    完成项目结构的搭建

--- src/main.js
+++ src/main.js
@@ -1,8 +1,18 @@
 import Vue from 'vue'
 import App from './App.vue'
+// 导入router
+import router from './router'
+// 导入element-ui
+import ElementUI from 'element-ui';
+// 导入element-uId样式
+import 'element-ui/lib/theme-chalk/index.css';
+// 使用element-ui
+Vue.use(ElementUI);
 
 Vue.config.productionTip = false
 
 new Vue({
   render: h => h(App),
+  // 将路由对象挂载到vue实例中
+  router
 }).$mount('#app')
---
+++ src/router/index.js
@@ -0,0 +1,15 @@
+// 导入vue
+import Vue from 'vue'
+// 导入vue-router
+import VueRouter from 'vue-router'
+// 使用vue-router
+Vue.use(VueRouter)
+// 创建一个新的vue-router对象
+var router=new VueRouter({
+    reader:[{
+
+    }]
+})
+
+// 将对象暴露出去
+export default router
